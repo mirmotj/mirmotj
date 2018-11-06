ce50d9952e428c209f685dd51b0f3852f91c9613
Jonathan M <Jonathan M@10.111.63.73>
2018-11-06 14:33:23 -0800

    added comments

--- gradeCalculator/GradeCalculator.java
+++ gradeCalculator/GradeCalculator.java
@@ -9,25 +9,26 @@ public class GradeCalculator {
 	
 	public static void math224 () {
 		
+		//Grade weights
 		int homeWgt = 6;
 		int quizWgt = 14;
 		int midWgt = 20;
 		int finWgt = 40;
 		
+		//Performance data
 		double[] homeScr = {10, 5.5, 0, 3, 0};
 		double[] quizScr = {8, 10};
+		double mid1Scr = (double)90 / 100;
+		double mid2Scr = (double)95 / 100;
+		double finScr = (double)95 / 100;
+		
+		//Calculation data
 		int homeTotal = 10;
 		int quizTotal = 10;
-		
 		double homeAvg = 0;
 		double quizAvg = 0;
 		
-		double mid1Scr = (double)90 / 100;
-		double mid2Scr = (double)85 / 100;
-		double finScr = (double)85 / 100;
-		
-		double totalGrade = 0;
-		
+		//Average homework grades
 		for(int i = 0; i < homeScr.length; i++) {
 			
 			homeAvg += (homeScr[i] / homeTotal);
@@ -35,6 +36,7 @@ public class GradeCalculator {
 		}
 		homeAvg = homeAvg / homeScr.length;
 		
+		//Average quiz grades
 		for(int i = 0; i < quizScr.length; i++) {
 			
 			quizAvg += (quizScr[i] / quizTotal);
@@ -42,7 +44,8 @@ public class GradeCalculator {
 		}
 		quizAvg = quizAvg / quizScr.length;
 		
-		totalGrade = (homeAvg * homeWgt) + (quizAvg * quizWgt)
+		//Total grade calculation
+		double totalGrade = (homeAvg * homeWgt) + (quizAvg * quizWgt)
 				+ (mid1Scr * midWgt) + (mid2Scr * midWgt) + (finScr * finWgt);
 		
 		System.out.println("Total Grade is: " + totalGrade);
