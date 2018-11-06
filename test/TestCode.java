2156ea4a8adc78182a01d2f8537ee0bc194702a3
Jonathan M <Jonathan M@10.111.63.73>
2018-11-06 13:19:16 -0800

    commit

---
+++ test/TestCode.java
@@ -0,0 +1,66 @@
+
+public class TestCode {
+
+	public static void main(String[] args) {
+		
+		//102418
+		//two counter strategy
+		/*int[] arr = {1, 3, 5, 7, 9};
+		int start = 0;
+		int end = arr.length - 1;
+		
+		while(start < end) {
+			int tmp = arr[start];
+			arr[start] = arr[end];
+			arr[end] = tmp;
+			start++;
+			end--;
+		}
+		
+		for(int i = 0; i < arr.length; i++) {
+			System.out.print(arr[i] + " ");
+		}*/
+		
+		//WIP
+		/*int[][] matrix = {{1, 2, 3},  {4, 5, 6}, {7, 8, 9}};
+		int[] tmp = new int[matrix.length];
+		int start = 0;
+		int end = matrix.length - 1;
+		
+		for(int i = 0; i < matrix.length; i++) {
+			while(start < end) {
+				tmp = matrix[start];
+				matrix[start] = matrix[end];
+				matrix[end] = tmp;
+				start++;
+				end--;
+			}
+		}
+		
+		for(int i = 0; i < matrix.length; i++) {
+			for(int j = 0; j < matrix[i].length; j++) {
+				System.out.print(matrix[i][j] + " ");
+			}
+			System.out.println();
+		}*/
+		
+		//102618
+		//git clone /url/
+		//git add -A
+		//git commit -m"comment"
+		//git push -u
+		//git
+		
+		//Get method
+		//Set method
+		
+		//Static variables
+		
+		//overloading
+		
+		//Unit testing
+		
+		
+		
+	}
+}
